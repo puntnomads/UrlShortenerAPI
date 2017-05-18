ab601808cf544207cf625724135f438bf855109c
Zakariya Moallin <puntnomads@gmail.com>
2017-05-18 16:58:41 +0000

    Added error for invalid URL

--- server.js
+++ server.js
@@ -1,53 +1,58 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var request = require('request');
 var base58 = require('./base58.js');
 var Url = require('./url');
 
-var url = process.env.MONGOLAB_URI;
+var url = 'mongodb://localhost:27017/url_shortener';
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
 var dbase = mongoose.connection;
 dbase.on('open', function () {
     dbase.db.listCollections().toArray(function (err, names) {
-        if (err){ console.log(err); };
+        if (err){ console.log(err); }
         var exists = false;
         names.forEach(function(entry) {
             if(entry.name==="counter"){
                 exists = true;
-            };
+            }
         });
         if(exists===false){
           dbase.collection('counter').insert({ _id: 'url_count', seq: 1 });  
-        };
+        }
     });
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.get('/new/*', function(req, res){
   var longUrl = req.params[0];
-  console.log(longUrl);
-  var shortUrl = '';
-  Url.findOne({long_url: longUrl}, function (err, doc){
-    if (err){ console.log(err); };  
-    if (doc){
-      shortUrl = req.headers.host  + "/" + base58.encode(doc._id);
-      res.send({'shortUrl': shortUrl});
+  request(longUrl, function (error, response, body) {
+    if (error) {
+      res.send({ 'error': error });
+      return;
     } else {
-      var newUrl = Url({
-        long_url: longUrl
-      });
-      newUrl.save(function(err) {
-        if (err){
-          console.log(err);
-        }
-        shortUrl = req.headers.host + "/" + base58.encode(newUrl._id);
-        res.send({'shortUrl': shortUrl});
-      });
-    }
+        var shortUrl = '';
+        Url.findOne({long_url: longUrl}, function (err, doc){
+          if (err){ console.log(err); } 
+          if (doc){
+            shortUrl = req.headers.host  + "/" + base58.encode(doc._id);
+            res.send({ 'original_url': longUrl, 'short_url': shortUrl });
+          } else {
+            var newUrl = Url({
+              long_url: longUrl
+            });
+            newUrl.save(function(err) {
+              if (err){ console.log(err); }
+              shortUrl = req.headers.host + "/" + base58.encode(newUrl._id);
+              res.send({ 'original_url': longUrl, 'short_url': shortUrl });
+            });
+          }
+        });
+      }
   });
 });
 
@@ -55,7 +60,7 @@ app.get('/:encoded_id', function(req, res){
   var base58Id = req.params.encoded_id;
   var id = base58.decode(base58Id);
   Url.findOne({_id: id}, function (err, doc){
-    if (err){};    
+    if (err){ console.log(err); }    
     if (doc) {
       res.redirect(doc.long_url);
     } else {
@@ -65,5 +70,5 @@ app.get('/:encoded_id', function(req, res){
 });
 
 app.listen(process.env.PORT || 8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Example app listening on port 8080!');
 });
